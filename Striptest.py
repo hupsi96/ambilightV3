57bdb8f5099c797d26436e2495afb9f40447147d
Christian Huppertz <>
2020-12-14 12:25:34 +0100

    tets

--- Striptest.py
+++ Striptest.py
@@ -17,7 +17,7 @@ num_pixels = 58
 ORDER = neopixel.GRBW
  
 pixels = neopixel.NeoPixel(
-    pixel_pin2, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
 )
  
  
