195ef58fe80e654e00ae6773e95606fcb2bd7711
Christian Huppertz <>
2020-12-14 08:56:02 +0100

    test

--- main.py
+++ main.py
@@ -19,25 +19,25 @@ def main():
 
     # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
     # NeoPixels must be connected to D10, D12, D18 or D21 to work.
-    pixel_pin = board.D18
+    #pixel_pin = board.D18
     
     # The number of NeoPixels
-    num_pixels = 58
+    #num_pixels = 58
     
     # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
     # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-    ORDER = neopixel.GRBW
+    #ORDER = neopixel.GRBW
     
-    strip = neopixel.NeoPixel(
-        pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
-    )
+    #strip = neopixel.NeoPixel(
+    #    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+    #)
     #test Strips
-    strip.fill((255,0,0,0))
-    strip.show()
-    time.sleep(2)
-    strip.fill((0,0,0,0))
-    strip.show()
-    time.sleep(2)
+    #strip.fill((255,0,0,0))
+    #strip.show()
+    #time.sleep(2)
+    #strip.fill((0,0,0,0))
+    #strip.show()
+    #time.sleep(2)
     
     
     #define States
