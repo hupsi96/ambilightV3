4881d266eb6db98a5d05cdb66cdfffefb402973a
Christian Huppertz <>
2020-12-14 10:35:13 +0100

    zrd

--- mqtt_client.py
+++ mqtt_client.py
@@ -130,7 +130,7 @@ class mqtt_client:
         
         global stripStorage
         
-        if msg.topic == "ambilightLamp/light/set" and msg.payload == "b'OFF'":
+        if msg.topic == "ambilightLamp/light/set":
             for i in range(len(strip)):
                 stripStorage[i] = strip[i]
                 print(stripStorage[i])
