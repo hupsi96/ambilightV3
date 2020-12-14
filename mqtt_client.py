e7824e0c849002c76179ed2ba7a5f9b464d84118
Christian Huppertz <>
2020-12-14 09:06:50 +0100

    test

--- mqtt_client.py
+++ mqtt_client.py
@@ -12,8 +12,25 @@ from States.RGB import RGB
 manager = Manager()
 managedRunning = manager.dict({'mqttRunning' : True})
 
-strip = None
-handler = None
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+
+# The number of NeoPixels
+num_pixels = 58
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRBW
+
+strip = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+white = White(strip)
+rgb = RGB(strip)
+off = Off(strip)
+handler = StateHandler(strip,[white,rgb,off])
 
 class mqtt_client:
 
@@ -29,8 +46,8 @@ class mqtt_client:
         
         #own Variables
         self.states = states
-        self.strip = strp
-        self.handler = StateHandler(strp, states)
+        #self.strip = strp
+        #self.handler = StateHandler(strp, states)
         
         #Testing
         self.test()
